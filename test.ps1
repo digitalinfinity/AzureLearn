7c0e81e7e322b1781633f6f586a4d05cda4a9ceb
Hitesh Kanwathirtha <hiteshk@microsoft.com>
2018-06-12 13:35:29 -0700

    Fixup

--- test.ps1
+++ test.ps1
@@ -2,7 +2,9 @@ $adsi = [ADSI]"WinNT://$Env:Computername"
 $Users = $adsi.Children  | where {$_.SchemaClassName  -eq 'user'}
 $shinobiUser = $users | Where-Object { $_.Name -eq "chakrashinobi" }
 if ($shinobiUser -ne $null) {
-    $edgePackage = get-appxpackage | where { $_.Name -eq "Microsoft.MicrosoftEdge" }
+    $packages = get-appxpackage
+    $packages | Select *
+    $edgePackage = $packages | where { $_.Name -eq "Microsoft.MicrosoftEdge" }
     $appLocalPath = Join-Path -Path $edgePackage.InstallLocation -ChildPath "microsoft.system.package.metadata\Application.Local"
     $appLocalFolder = New-Item -ItemType Directory -Force -Path $appLocalPath
 
