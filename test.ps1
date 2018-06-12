d69a7bdfd9cfa5af3edb396384e4c1b0efa33401
Hitesh Kanwathirtha <hiteshk@microsoft.com>
2018-06-12 13:30:41 -0700

    Try application of acl

--- test.ps1
+++ test.ps1
@@ -1,3 +1,13 @@
-([System.Security.Principal.WindowsIdentity]::GetCurrent()) | Format-List
+$adsi = [ADSI]"WinNT://$Env:Computername"
+$Users = $adsi.Children  | where {$_.SchemaClassName  -eq 'user'}
+$shinobiUser = $users | Where-Object { $_.Name -eq "chakrashinobi" }
+if ($shinobiUser -ne $null) {
+    $edgePackage = get-appxpackage | where { $_.Name -eq "Microsoft.MicrosoftEdge" }
+    $appLocalPath = Join-Path -Path $edgePackage.InstallLocation -ChildPath "microsoft.system.package.metadata\Application.Local"
+    $appLocalFolder = New-Item -ItemType Directory -Force -Path $appLocalPath
 
-Write-Host IsAdmin: ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $acl = Get-Acl $appLocalPath
+    $accessRule = New-Object System.Security.AccessControl.FilesystemAccessRule($shinobiUser.Name, "FullControl", "Allow")
+    $acl.SetAccessRule($accessRule)
+    Set-Acl $appLocalPath $acl
+}
